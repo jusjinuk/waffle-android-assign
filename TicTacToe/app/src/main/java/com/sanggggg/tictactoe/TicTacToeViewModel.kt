b8eba1f676c942e3f26b805c6c1d4d8e7b77c3c6
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-09-11 13:29:16 +0900

    Assignment1 Done. (Added diagonal logic, draw/win logic)

--- TicTacToe/app/src/main/java/com/sanggggg/tictactoe/TicTacToeViewModel.kt
+++ TicTacToe/app/src/main/java/com/sanggggg/tictactoe/TicTacToeViewModel.kt
@@ -16,6 +16,10 @@ class TicTacToeViewModel : ViewModel() {
     private val STATUS_X_WIN = "PLAYER X WIN!"
     private val STATUS_DRAW = "DRAW!"
 
+    // CONSTANTS added
+    private val STATUS_O_TURN = "PLAYER O TURN"
+    private val STATUS_X_TURN = "PLAYER X TURN"
+
     // recommended LiveData fields (fill free to fix it)
     val cells = ArrayList<MutableLiveData<String>>()
     val gameStatus = MutableLiveData<String>()
@@ -36,6 +40,11 @@ class TicTacToeViewModel : ViewModel() {
         if (cells[pos].value == PLAYER_X || cells[pos].value == PLAYER_O) {
             return
         }
+        if (whoseTurn){
+            gameStatus.value = STATUS_X_TURN
+        } else{
+            gameStatus.value = STATUS_O_TURN
+        }
         if (whoseTurn) {
             cells[pos].value = PLAYER_O
         } else {
@@ -44,15 +53,16 @@ class TicTacToeViewModel : ViewModel() {
         turn++
         checkWinOrNot()
         whoseTurn = !whoseTurn
-
     }
 
     fun restart() {
-        gameStatus.value = STATUS_PLAYING
+        gameStatus.value = STATUS_O_TURN
         for (i in 0..8) {
             cells[i].value = ""
         }
+        whoseTurn = true
         done = false
+        turn = 0
     }
 
     fun checkWinOrNot() {
@@ -60,9 +70,15 @@ class TicTacToeViewModel : ViewModel() {
             if ((cells[i].value != "" &&
                         cells[i].value == cells[i + 3].value &&
                         cells[i + 3].value == cells[i + 6].value) ||
-                (cells[i*3].value != "" &&
+                (cells[i * 3].value != "" &&
                         cells[i * 3].value == cells[i * 3 + 1].value &&
-                        cells[i * 3 + 1].value == cells[i * 3 + 2].value)
+                        cells[i * 3 + 1].value == cells[i * 3 + 2].value) ||
+                (cells[4].value != "" &&
+                        cells[0].value == cells[4].value &&
+                        cells[4].value == cells[8].value) ||
+                (cells[4].value != "" &&
+                        cells[2].value == cells[4].value &&
+                        cells[4].value == cells[6].value)
             ) {
                 if (whoseTurn) {
                     gameStatus.value = STATUS_O_WIN
@@ -72,7 +88,11 @@ class TicTacToeViewModel : ViewModel() {
                 done = true
                 return
             }
-
+        }
+        if (turn == 9) {
+            gameStatus.value = STATUS_DRAW
+            done = true
+            return
         }
     }
 }
